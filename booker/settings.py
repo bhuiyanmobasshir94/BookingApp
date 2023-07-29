212875ca673ec46e7fa2e437de974d0dff71876c
bhuiyanmobasshir94 <mobasshirbhuiyan.shagor@gmail.com>
2023-07-29 12:02:41 +0600

    Updated settings

--- booker/settings.py
+++ booker/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "booker.booking.BookingConfig"
+    "booker.booking.apps.BookingConfig"
 ]
 
 MIDDLEWARE = [
