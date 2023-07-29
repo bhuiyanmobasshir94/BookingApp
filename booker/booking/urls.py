430dcc2f3da4c2987e49764b3345c16899a4502f
AbuArefinArooop <aroop.cse@gmail.com>
2023-07-29 09:56:53 +0300

    template updated

--- booker/booking/urls.py
+++ booker/booking/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("", SlotPageView.as_view()),
+    path("",book_slot),
 
 ]
--- booker/booking/views.py
+++ booker/booking/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 from django.views.generic import TemplateView
 from .models import *
 
 
 # Create your views here.
-class SlotPageView(TemplateView):
-    template_name = "slot.html"
+# class SlotPageView(TemplateView):
+#     template_name = "slot.html"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["slots"] = Slot.objects.all()
-        return context
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context["slots"] = Slot.objects.all()
+#         return context
+def book_slot(request):
+    slots = Slot.objects.all()
+    return render(request, 'slot.html', {'slots': slots}) 
